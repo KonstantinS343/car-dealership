985b54daadd6de8115579172d81f5a4464590816
KonstantinS343 <mihailrudkovic@gmail.com>
2023-11-20 15:56:28 +0300

    fix buyer admin

--- src/apps/buyer/admin.py
+++ src/apps/buyer/admin.py
@@ -1,11 +1,5 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
 
 from .models import Buyer
 
-
-class BuyerAdmin(UserAdmin):
-    list_display = UserAdmin.list_display + ('update_at',)
-
-
-admin.site.register(Buyer, BuyerAdmin)
+admin.site.register(Buyer)
