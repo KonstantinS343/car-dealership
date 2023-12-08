3902aff047d607fdb5f16b5ce19930d8f07cd65f
KonstantinS343 <mihailrudkovic@gmail.com>
2023-12-08 12:43:34 +0300

    fix query set func

--- src/apps/purchase_history/model/queryset.py
+++ src/apps/purchase_history/model/queryset.py
@@ -15,11 +15,14 @@ class PurchasesSalesHistoryQuerySet(models.QuerySet):
     def supplier_history(self, id) -> models.Manager[models.Model]:
         return self.filter(supplier_id=id, is_active=True)
 
-    def get_sales_profit(self, id) -> models.Manager[models.Model]:
-        return self.filter(supplier_id=id, is_active=True).aggregate(total_profit=models.Sum('final_price'))
+    def get_sales_profit(self, id, type='supplier') -> models.Manager[models.Model]:
+        queryset = self.filter(supplier_id=id, is_active=True) if type == 'supplier' else self.filter(car_dealership=id, is_active=True)
+        return queryset.aggregate(total_profit=models.Sum('final_price'))
 
-    def get_sold_cars_amount(self, id) -> models.Manager[models.Model]:
-        return self.filter(supplier_id=id, is_active=True).select_related('car_model').annotate(cars_amount=models.Count('car_model'))
+    def get_sold_cars_amount(self, id, type='supplier') -> models.Manager[models.Model]:
+        queryset = self.filter(supplier_id=id, is_active=True) if type == 'supplier' else self.filter(car_dealership=id, is_active=True)
+        return queryset.select_related('car_model').annotate(cars_amount=models.Count('car_model'))
 
-    def get_sold_cars_profit(self, id) -> models.Manager[models.Model]:
-        return self.filter(supplier_id=id, is_active=True).select_related('car_model').annotate(final_model_profit=models.Sum('final_price'))
+    def get_sold_cars_profit(self, id, type='supplier') -> models.Manager[models.Model]:
+        queryset = self.filter(supplier_id=id, is_active=True) if type == 'supplier' else self.filter(car_dealership=id, is_active=True)
+        return queryset.annotate(final_model_profit=models.Sum('final_price'))
