4d1c89ddc5187944c5dab640a373d45f7ec6299d
KonstantinS343 <mihailrudkovic@gmail.com>
2023-12-13 11:27:46 +0300

    add fill db logic

--- src/apps/common/management/commands/fill.py
+++ src/apps/common/management/commands/fill.py
@@ -1,11 +1,12 @@
 from django.core.management.base import BaseCommand
+from django.db import transaction
 
 import random
-from typing import List, Dict
+from typing import List, Dict, Tuple, Any
 
 from apps.buyer.model.models import Buyer
 from apps.car_model.model.models import Car
-from apps.car_show.model.models import CarShow
+from apps.car_show.model.models import CarShow, UniqueBuyersCarDealership
 from apps.common.models import User
 from apps.supplier.model.models import Supplier, SupplierCarModel
 
@@ -24,25 +25,28 @@ class Command(BaseCommand):
     CAR_BRANDS = ('BMW', 'Hyundai', 'Honda', 'Ford', 'Audi', 'Volkswagen', 'Mercedes-Benz')
 
     @staticmethod
-    def gen_user() -> List[Supplier]:
+    def gen_user() -> Tuple[List[Supplier], List[Buyer]]:
         """
         Функция создание пользователей, покупателей, поставщиков.
         """
         suppliers_list = []
+        buyer_list = []
         for i in range(10):
             buyer_user = User.objects.create_user(username=f'client_{i}', password='password', user_type=1, email_confirmed=True)
             supplier_user = User.objects.create_user(username=f'supplier_{i}', password='password', user_type=3, email_confirmed=True)
             supplier = Supplier.objects.create(user_id=supplier_user.id, name='Supplier Name', country='RU', year_foundation=1990, buyer_amount=100)
-            Buyer.objects.create(user_id=buyer_user.id, balance=gen_float(100000.0, 1000000.0))
+            buyer = Buyer.objects.create(user_id=buyer_user.id, balance=gen_float(100000.0, 1000000.0))
             suppliers_list.append(supplier)
-        return suppliers_list
+            buyer_list.append(buyer)
+        return suppliers_list, buyer_list
 
     @staticmethod
-    def gen_carshow() -> List[Dict[str, str | float]]:
+    def gen_carshow() -> Tuple[List[Dict[str, Any]], List[CarShow]]:
         """
         Функция создание автосалонов.
         """
         cars_specifications = []
+        carshow_list = []
         for i in range(5):
             carshow_user = User.objects.create_user(username=f'carshow_owner_{i}', password='password', user_type=2, email_confirmed=True)
             cars_specifications.append(
@@ -54,10 +58,11 @@ class Command(BaseCommand):
                     'car_body': random.choice(CarShow.CAR_BODY_TYPE)[0],
                 }
             )
-            CarShow.objects.create(
+            carshow = CarShow.objects.create(
                 user_id=carshow_user.id, name=f'Car Show Name {i}', country='RU', balance=gen_float(1000000.0, 20000000.0), **cars_specifications[-1]
             )
-        return cars_specifications
+            carshow_list.append(carshow)
+        return cars_specifications, carshow_list
 
     @staticmethod
     def gen_cars(cars_specifications: List[Dict[str, str | float]], suppliers: List[Supplier]) -> None:
@@ -68,7 +73,17 @@ class Command(BaseCommand):
             car = Car.objects.create(brand=random.choice(Command.CAR_BRANDS), **random.choice(cars_specifications))
             SupplierCarModel.objects.create(supplier=random.choice(suppliers), car_model=car, price=gen_float(30000.0, 200000.0))
 
+    @staticmethod
+    def gen_unique_buyers(buyers: List[Buyer], carshows: List[CarShow]) -> None:
+        """
+        Функция для генерации постоянных клиентов автосална.
+        """
+        for i in range(5):
+            UniqueBuyersCarDealership.objects.create(car_dealership=random.choice(carshows), buyer=random.choice(buyers))
+
+    @transaction.atomic
     def handle(self, *args, **kwargs):
-        suppliers = Command.gen_user()
-        cars_specifications = Command.gen_carshow()
+        suppliers, buyers = Command.gen_user()
+        cars_specifications, carshows = Command.gen_carshow()
         Command.gen_cars(cars_specifications=cars_specifications, suppliers=suppliers)
+        Command.gen_unique_buyers(buyers=buyers, carshows=carshows)
